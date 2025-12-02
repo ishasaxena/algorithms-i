1f8f48c0ab3577f85e0e27b21cd20b7f9b2e1ff2
iamparwinder <iamparwinder@gmail.com>
2025-12-02 19:53:18 +0530

    initial project setup

---
+++ src/main/java/com/isha/algorithms_i/AlgorithmsIApplication.java
@@ -0,0 +1,13 @@
+package com.isha.algorithms_i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isha/algorithms_i/AlgorithmsIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isha.algorithms_i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
